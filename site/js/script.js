c24a4dd99cf258f936bf31c5c7e3d561b3d938ff
Komal Habura5 <komalhabura5@gmail.com>
2021-04-24 10:37:39 +0530

    myscript

---
+++ site/js/script.js
@@ -0,0 +1,8 @@
+var array=["j","k","j"]
+for(var i=0;i<array.length;i++){
+    if(array[i]=="j"){
+        console.log("Goodbye JSomeName");
+    }else{
+        console.log("Hello SomeName");
+    }
+}
